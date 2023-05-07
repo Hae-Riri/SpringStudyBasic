debfd688765859d267e7c7d601757fba3fbf8e21
Hae-Riri <ehl3288@naver.com>
2023-05-07 20:00:31 +0900

    chap7 의존관게 자동주입 : 수동 빈 등록 삭제

--- src/main/java/com/example/springstudybasic/core/AutoAppConfig.java
+++ src/main/java/com/example/springstudybasic/core/AutoAppConfig.java
@@ -1,8 +1,5 @@
 package com.example.springstudybasic.core;
 
-import com.example.springstudybasic.core.member.MemberRepository;
-import com.example.springstudybasic.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,8 +9,4 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }
