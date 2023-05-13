9017d2ec8ef3e87bd19ef2776ab9f6aa4d8106b8
Hae-Riri <ehl3288@naver.com>
2023-05-13 17:21:40 +0900

    chap8 빈 생명주기 콜백

---
+++ src/main/java/com/example/springstudybasic/core/lifecycle/NetworkClient.java
@@ -0,0 +1,43 @@
+package com.example.springstudybasic.core.lifecycle;
+
+import lombok.NoArgsConstructor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화연결메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
